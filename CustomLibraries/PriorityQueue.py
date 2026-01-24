25076df4b2cd3a9955455bd450ec19964d74cfd8
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-01-24 19:10:18 +0300

    Added comments for some methods.

--- CustomLibraries/PriorityQueue.py
+++ CustomLibraries/PriorityQueue.py
@@ -3,11 +3,8 @@ class PriorityQueue:
     This class is a concrete implementation of the priority queue ADT.
     It uses a heap as the underlying structure to achive the best overall performance accross all operations
 
-    Objects enqueue to this queue must have a method called get_key that returns the objects priority and all
-    methods are based on the assumption all elements of the queue implement this method.
-
-    The queue functions as a max heap by default by can be change to to behave as a min heap during initialization by passing 
-    in a different function that computes the priority
+    The class can function both as a max and min heap based on the function provided and hence it is up to the 
+    user to provide the function that computes the priority
 
     """
 
@@ -15,18 +12,25 @@ class PriorityQueue:
         pass
 
     def __init__(self, priority_function, iter: list = []) -> None:
-
+        
         self._size = len(iter)
         self._priority = priority_function
         self._heap = iter
         if self._heap:
             self._heapify()
+
+
+    def peek(self):
+        # returns a reference of the highest priority object
+        if self._size > 0:
+            return self._heap[0]
+        raise self.QueueException("Queue is empty")
             
 
 
 
     def _heapify(self) -> None:
-  
+        # constructs a valid heap
         last_parent_index = (self._size - 2) // 2
 
         for i in range(last_parent_index,-1,-1):
@@ -34,12 +38,14 @@ class PriorityQueue:
 
 
     def enqueue(self, obj) -> None:
+        # adds an object to the queue
         self._heap.append(obj)
         self._size +=1
         self._bubble_up(self._size -1)
 
 
     def _bubble_up(self, index: int) -> None:
+        # positions the object with the given index to its rightful place within the queue/ heap.
 
         new = self._heap[index]
 
@@ -57,6 +63,7 @@ class PriorityQueue:
 
 
     def dequeue(self):
+        # removes the object with the highest priority from the queue and returns it (else raises an error if the queue is empty)
         if self._heap:
             high = self._heap[0]
             self._heap[0] = self._heap.pop()
@@ -68,6 +75,7 @@ class PriorityQueue:
 
     
     def _bubble_down(self, index: int) -> None:
+        # positions the object with the given index to its rightful place within the queue/ heap.
         elem = self._heap[index]
 
         while True:
@@ -94,6 +102,8 @@ class PriorityQueue:
 
 
     def _highest_priority_child_index(self, parent_index: int) -> int:
+        # returns the index of the child with the highest priority given the index of the parent (if the parent is a leaf node it returns -1)
+        
         left_child_index = 2 * parent_index + 1
         right_child_index = 2* parent_index + 2
         last_index = self._size -1
