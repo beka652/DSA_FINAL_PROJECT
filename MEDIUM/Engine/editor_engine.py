173e2081ba1a4d512586885b5cedbcb9f9afdd60
Natilegend <natanelfikire11@gmail.com>
2026-01-31 21:36:57 +0300

    Fixed module not found error for medium project

--- MEDIUM/Engine/editor_engine.py
+++ MEDIUM/Engine/editor_engine.py
@@ -1,5 +1,8 @@
-from .models import Node, Action
 import time
+from turtle import clear
+
+from .model import *
+
 
 class TextEditor:
     def __init__(self):
@@ -7,43 +10,45 @@ class TextEditor:
         self.tail = Node(None)
         self.head.next = self.tail
         self.tail.prev = self.head
-        
-        self.cursor = self.tail  
+
+        self.cursor = self.tail
         self.undo_stack = []
         self.redo_stack = []
-        
-    """below this are internal helper """     
+
+    """below this are internal helper """
+
     def _insert_node(self, char, position_node):
-            new_node = Node(char)
-            prev_node = position_node.prev
-            
-            new_node.next = position_node
-            new_node.prev = prev_node
-            prev_node.next = new_node
-            position_node.prev = new_node
-            return new_node
-        
+        new_node = Node(char)
+        prev_node = position_node.prev
+
+        new_node.next = position_node
+        new_node.prev = prev_node
+        prev_node.next = new_node
+        position_node.prev = new_node
+        return new_node
+
     def _remove_node(self, node):
-            if node.value is None: return None
-            p, n = node.prev, node.next
-            p.next = n
-            n.prev = p
-            return node.value
+        if node.value is None:
+            return None
+        p, n = node.prev, node.next
+        p.next = n
+        n.prev = p
+        return node.value
 
     def _handle_batching(self, action_type, char):
-            if self.undo_stack and self.undo_stack[-1].can_batch(action_type):
-                self.undo_stack[-1].data.append(char)
-                self.undo_stack[-1].timestamp = time.time()
-            else:
-                new_action = Action(action_type)
-                new_action.data.append(char)
-                self.undo_stack.append(new_action)
+        if self.undo_stack and self.undo_stack[-1].can_batch(action_type):
+            self.undo_stack[-1].data.append(char)
+            self.undo_stack[-1].timestamp = time.time()
+        else:
+            new_action = Action(action_type)
+            new_action.data.append(char)
+            self.undo_stack.append(new_action)
 
     def write(self, char, is_undoing=False):
         if not is_undoing:
             self.redo_stack.clear()
-            self._handle_batching('WRITE', char)
-            
+            self._handle_batching("WRITE", char)
+
         self._insert_node(char, self.cursor)
 
     def delete(self, is_undoing=False):
@@ -52,23 +57,24 @@ class TextEditor:
             return
 
         char = self._remove_node(target)
-        
+
         if not is_undoing:
             self.redo_stack.clear()
-            self._handle_batching('DELETE', char)
+            self._handle_batching("DELETE", char)
 
     def move_cursor(self, direction):
         if direction == "left" and self.cursor.prev != self.head:
             self.cursor = self.cursor.prev
         elif direction == "right" and self.cursor != self.tail:
             self.cursor = self.cursor.next
-    
+
     def undo(self):
-        if not self.undo_stack: return
+        if not self.undo_stack:
+            return
         action = self.undo_stack.pop()
         self.redo_stack.append(action)
 
-        if action.type == 'WRITE':
+        if action.type == "WRITE":
             for _ in range(len(action.data)):
                 self._remove_node(self.cursor.prev)
         else:
@@ -76,11 +82,12 @@ class TextEditor:
                 self._insert_node(char, self.cursor)
 
     def redo(self):
-        if not self.redo_stack: return
+        if not self.redo_stack:
+            return
         action = self.redo_stack.pop()
         self.undo_stack.append(action)
 
-        if action.type == 'WRITE':
+        if action.type == "WRITE":
             for char in action.data:
                 self._insert_node(char, self.cursor)
         else:
