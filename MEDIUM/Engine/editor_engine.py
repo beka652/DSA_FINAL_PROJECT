6da27b7817cdbd4ccd3d9dd49849ebc1ee0253ef
Natilegend <natanelfikire11@gmail.com>
2026-01-31 17:29:09 +0300

    create an internal methods to perform tasks in the back and the user can't interact with.

--- MEDIUM/Engine/editor_engine.py
+++ MEDIUM/Engine/editor_engine.py
@@ -1,4 +1,5 @@
-from .models import Node
+from .models import Node, Action
+import time
 
 class TextEditor:
     def __init__(self):
@@ -10,3 +11,29 @@ class TextEditor:
         self.cursor = self.tail  
         self.undo_stack = []
         self.redo_stack = []
+    """below this are internal helper """     
+    def _insert_node(self, char, position_node):
+            new_node = Node(char)
+            prev_node = position_node.prev
+            
+            new_node.next = position_node
+            new_node.prev = prev_node
+            prev_node.next = new_node
+            position_node.prev = new_node
+            return new_node
+        
+    def _remove_node(self, node):
+            """Internal helper to remove a specific node."""
+            if node.value is None: return None
+            p, n = node.prev, node.next
+            p.next = n
+            n.prev = p
+            return node.value
+
+    def _handle_batching(self, action_type, char):
+            if self.undo_stack and self.undo_stack[-1].can_batch(action_type):
+                self.undo_stack[-1].data.append(char)
+                self.undo_stack[-1].timestamp = time.time()
+            else:
+                new_action = Action(action_type)
+                self.undo_stack.append(new_action)
